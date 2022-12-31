80d5febef9a5d32dc97ef848e962eac624613ab4
rafael <rafaelfb28.rb@gmail.com>
2022-12-31 17:22:27 -0300

    Novos

--- organizador_pre_bkp.sh
+++ organizador_pre_bkp.sh
@@ -2,7 +2,7 @@
 
 hoje=$(date +%d-%m-%y)
 
-for diretorio in `find /BKP_DEDICADOS/ -name "BKP*" | grep -v tar* | cut -d'/' -f3 | sort`;
+for diretorio in `ls -l /BKP_DEDICADOS/ | grep BKP_ | grep -v tar.gz | cut -d':' -f2 | cut -d' ' -f2`;
 do
 	#Inicio do BKP da pasta desenvolvimento
         if [ $diretorio == 'BKP_DESENVOLVIMENTO' ];
@@ -12,7 +12,7 @@ do
                 mkdir $hoje
                 chmod -R 777 $hoje
                 cp -r /dados/samba/desenvolvimento/* $hoje
-                find -mtime +2 -delete
+		find -mtime +1 -delete
 	#Inicio do BKP do banco da LOCAWEB
         elif [ $diretorio == 'BKP_LOCAWEB' ];
         then
@@ -21,7 +21,7 @@ do
                 mkdir $hoje
                 chmod -R 777 $hoje
                 PGPASSWORD="cr10100306@"  /usr/bin/pg_dump --host saam_clientes.postgresql.dbaas.com.br --port 5432 --username "saam_clientes" --format custom --blobs --verbose --file "/BKP_DEDICADOS/$diretorio/$hoje/saam_clientes.backup" "saam_clientes"
-                find -mtime +2 -delete
+		find -mtime +1 -delete
 	#Inicio do BKP do banco SPEDAO
         elif [ $diretorio == 'BKP_SPEDAO' ];
         then
@@ -30,14 +30,14 @@ do
                 mkdir $hoje
                 chmod -R 777 $hoje
                 PGPASSWORD="10100306@"  /usr/lib/postgresql/9.6/bin/pg_dump --host localhost --port 5432 --username "sisaudconadaoavestruz@0620181.0.31" --format custom --blobs --verbose --file "/BKP_DEDICADOS/$diretorio/$hoje/sped.backup" "sped"
-                find -mtime +2 -delete
+		find -mtime +1 -delete
 	#Inicio da criação das pastas de BKP dos servidores DEDICADOS OBS(O SCP responsavel por jogar o BKP nas pastas está em cada dedicado)
         else
                 cd /BKP_DEDICADOS/$diretorio/
-                rm -rf $hoje
-                mkdir $hoje
-                chmod -R 777 $hoje
-                find -mtime +2 -delete
+        #        rm -rf $hoje
+        #        mkdir $hoje
+        #        chmod -R 777 $hoje
+		#find -mtime +1 -delete
         fi
 done
 
--- secundario.sh
+++ secundario.sh
@@ -20,11 +20,11 @@ echo "$(date +%H:%M:%S) - Copiando 'webswing.config'" >> /home/bkp_dedicado/$por
 sshpass -p "user_bkp_2022" scp -r -P 4922 /home/webswing/webswing.config user_bkp@189.15.3.32:/BKP_DEDICADOS/$diretorio_remoto/$(date +%d-%m-%y);
 echo "" >> /home/bkp_dedicado/$porta.log;
 echo "$(date +%H:%M:%S) - Criando a pasta $porta" >> /home/bkp_dedicado/$porta.log;
-mkdir $diretorio_mnt/$porta &>> /home/bkp_dedicado/$porta.log;
-cd $diretorio_mnt/$porta &>> /home/bkp_dedicado/$porta.log;
+mkdir $diretorio_mnt/$(date +%d-%m-%y)/$porta &>> /home/bkp_dedicado/$porta.log;
+cd $diretorio_mnt/$(date +%d-%m-%y)/$porta &>> /home/bkp_dedicado/$porta.log;
 echo "" >> /home/bkp_dedicado/$porta.log;
 echo "$(date +%H:%M:%S) - Limpando LOG WAL antigo" >> /home/bkp_dedicado/$porta.log;
-/usr/lib/postgresql/12/bin/pg_archivecleanup "${DIR_LOGS}" "${LAST_LOG}"
+/usr/lib/postgresql/$versao/bin/pg_archivecleanup "${DIR_LOGS}" "${LAST_LOG}"
 echo "" >> /home/bkp_dedicado/$porta.log;
 echo "$(date +%H:%M:%S) - Realizando o backup" >> /home/bkp_dedicado/$porta.log;
 PGPASSWORD="10100306@" /usr/lib/postgresql/$versao/bin/pg_basebackup -h127.0.0.1 -p $porta -U sisaudconadaoavestruz@0620181.0.31 -D backup -Ft -z -P &>> /home/bkp_dedicado/$porta.log;
@@ -33,13 +33,13 @@ if [ $var == 0 ];
 then
 	echo "" >> /home/bkp_dedicado/$porta.log;
 	echo "$(date +%H:%M:%S) - Backup realizado com sucesso" >> /home/bkp_dedicado/$porta.log;
-	echo "" >> /home/bkp_dedicado/$porta.log;
-	cd ..;		
-	echo "$(date +%H:%M:%S) - Download da porta para o servidor local" >> /home/bkp_dedicado/$porta.log;
-    sshpass -p "user_bkp_2022" scp -rp -P 4922 $porta user_bkp@189.15.3.32:/BKP_DEDICADOS/$diretorio_remoto/$(date +%d-%m-%y) &>> /home/bkp_dedicado/$porta.log;	
-	echo "" >> /home/bkp_dedicado/$porta.log;
-	echo "$(date +%H:%M:%S) - Removendo porta" >> /home/bkp_dedicado/$porta.log;
-	rm -rf $porta &>> /home/bkp_dedicado/$porta.log;
+	#echo "" >> /home/bkp_dedicado/$porta.log;
+	#cd ..;		
+	#echo "$(date +%H:%M:%S) - Download da porta para o servidor local" >> /home/bkp_dedicado/$porta.log;
+    #sshpass -p "user_bkp_2022" scp -rp -P 4922 $porta user_bkp@189.15.3.32:/BKP_DEDICADOS/$diretorio_remoto/$(date +%d-%m-%y) &>> /home/bkp_dedicado/$porta.log;	
+	#echo "" >> /home/bkp_dedicado/$porta.log;
+	#echo "$(date +%H:%M:%S) - Removendo porta" >> /home/bkp_dedicado/$porta.log;
+	#rm -rf $porta &>> /home/bkp_dedicado/$porta.log;
 elif [ $var == 1 ] || [ $var ==2 ]:
 then
 	echo "" >> /home/bkp_dedicado/$porta.log;
